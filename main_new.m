a4b9c1cfdc75851bab1c2be07ab92f0e1971782f
Bailamos <bailamos@github.com>
2018-10-11 23:53:41 +0200

    fix

--- main_new.m
+++ main_new.m
@@ -1,6 +1,8 @@
+clear;
 lines_start = 1;
-lines_skip = 5;
+lines_skip = 1;
 lines_end = 140;
+x_axis = lines_start:lines_skip:lines_end;
 
 load("nbss.mat");
 data = vbss;
@@ -19,7 +21,6 @@ for image_number = 1:4;
   v_var = var(x);
   v_skewness  = skewness(x);
 
-  x_axis = lines_start:lines_skip:lines_end;
   plot(x_axis, v_avg, 'color', 'red');
   hold on;
   plot(x_axis, v_std, 'color', 'blue');
@@ -34,34 +35,27 @@ for image_number = 1:4;
   
   legend('avg','std','max','min','var','skewness')   
   figure();
+
+  
+  v_avg =  vec2mat(v_avg, 10);
+  skewness_avg(image_number, :) = skewness(v_avg');
+  
+  v_std =  vec2mat(v_std, 10);
+  skewness_std(image_number, :) = skewness(v_std');
+  
+  v_max =  vec2mat(v_max, 10);
+  skewness_max(image_number, :) = skewness(v_max');
+  
+  v_min =  vec2mat(v_min, 10);
+  skewness_min(image_number, :) = skewness(v_min');
 end
 
-##for image_number = 1:4;
-##
-##  x = data(image_number, 1:600, lines_start:lines_end);
-##  x = squeeze(x);
-##  x = x';
-##  v_avg = mean(x);
-##  v_std = std(x);
-##  v_max = max(x);
-##  v_min = min(x);
-##  v_var = var(x);
-##  v_skewness  = skewness(x);
-##
-##  plot(1:600, v_avg, 'color', 'red');
-##  hold on;
-##  plot(1:600, v_std, 'color', 'blue');
-##  hold on;
-##  plot(1:600, v_max, 'color', 'green');
-##  hold on;
-##  plot(1:600, v_min, 'color', 'black');
-##  hold on;
-##  plot(1:600, v_var, 'color', 'cyan');
-##  hold on;
-##  plot(1:600, v_skewness, 'color', 'yellow');
-##  
-##  legend('avg','std','max','min','var','skewness')
-##  
-##  figure();
-##end
+scatter(skewness_min(1,:), skewness_std(1,:))
+hold on;
+scatter(skewness_min(2,:), skewness_std(2,:))
+hold on;
+scatter(skewness_min(3,:), skewness_std(3,:))
+hold on;
+scatter(skewness_min(4,:), skewness_std(4,:))
+
 
