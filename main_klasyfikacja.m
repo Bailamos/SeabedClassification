dcdaca283ba2edef68cd336c65cbf82c6f481405
bailamos <bailamos@github.com>
2018-11-01 00:57:21 +0100

    cleanup

--- main_klasyfikacja.m
+++ main_klasyfikacja.m
@@ -17,38 +17,5 @@ for i = 1 : 4
 endfor
 
 
-plotClassificationData(classification_params, colors);
-figure();
-plotClasses(nn_classes, colors);
-
 #testing section
-for i = 1 : 4
-  test_classification_params(i,:,:) = calculateClassificationParams(test_set(i,:,:));  
-  current_test_classification_params = squeeze(test_classification_params(i,:,:));  
-  
-  classes = [];
-  for j = 1 : 400
-    distance_1 = distanceBetweenPoints(...
-                    current_test_classification_params(j,:),
-                    nn_classes(1,:));
-    distance_2 = distanceBetweenPoints(...
-                    current_test_classification_params(j,:),
-                    nn_classes(2,:));
-    distance_3 = distanceBetweenPoints(...
-                    current_test_classification_params(j,:),
-                    nn_classes(3,:));
-    distance_4 = distanceBetweenPoints(...
-                    current_test_classification_params(j,:),
-                    nn_classes(4,:));
-                    
-    [a class] = min([distance_1 distance_2 distance_3 distance_4]);
-    classes = [classes; class];     
-  endfor
-
-  classification_result(i,:) = classes;
-  current_test_classification_params = [current_test_classification_params, classes];
-  
-  plotTestData(current_test_classification_params, colors);
-endfor
-
-saveClassificationResultsToFile(classification_result);
+NNClassification(test_set, classification_params, nn_classes, colors);
