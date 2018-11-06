a207b31bddccb92c1d5c175afb8f35fdee99da0c
bailamos <bailamos@github.com>
2018-11-06 14:27:45 +0100

    rozne rzuty

--- plotClassificationData.m
+++ plotClassificationData.m
@@ -14,6 +14,17 @@ function retval = plotClassificationData (data, colors)
     "piasek drobnoziarnisty",...
     "piasek gruboziarnisty"});
   legend show;
-  title ("Rozklad dla paraemtrow: entropia, wartosc maksymalna oraz nacyhlenie autokorelacji");
+  title (["Rozklad dla parametrow:"...
+         "entropia, wartosc maksymalna oraz nacyhlenie autokorelacji"]);
   view(-100,30)
+  print -djpg izmometryczny.jpg
+  #entropia/naychlenie
+  #view(90,0)
+  #print -djpg rzut1.jpg
+  #entropia/wartosc maksymalna
+  #view(0,90)
+  #print -djpg rzut2.jpg
+  #nacyhlenie/wartosc maksymalna 
+  #view(0,0)
+  #print -djpg rzut3.jpg
 endfunction
